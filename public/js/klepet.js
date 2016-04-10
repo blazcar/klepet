8b04a70e579afc3dee0e498be1b93d514bcab0c6
Blaž Čar <bc0129@student.uni-lj.si>
2016-04-10 17:22:20 +0000

    Zaključek dela na IV. nalogi

--- public/js/klepet.js
+++ public/js/klepet.js
@@ -43,6 +43,19 @@ Klepet.prototype.procesirajUkaz = function(ukaz) {
         sporocilo = 'Neznan ukaz';
       }
       break;
+    case 'dregljaj':
+      besede.shift();
+      var vzdevek = besede.join(' ');
+      var besedilo = besede.join(' ');
+      var parametri = besedilo.split('\"');
+      if (parametri != null) {
+       this.socket.emit('dregljaj',  { vzdevek: vzdevek});
+       sporocilo = 'Dregljaj za ' + vzdevek;
+      } else {
+       sporocilo = 'Neznan ukaz';
+      }
+      
+      break;
     default:
       sporocilo = 'Neznan ukaz.';
       break;
